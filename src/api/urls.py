d6d3fb8f5f384a977c762d477e80bf5c19a389ec
Sara Raicevic <sara.raicevic@citrus.codes>
2025-12-11 20:55:26 +0100

    Add endpoint to fetch all destionations

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from src.api.views.authentication import LoginView, ChangePasswordView
+from src.api.views.company import CompanyDestinationsView
 
 urlpatterns = [
-    path("auth/login/", LoginView.as_view(), name="login"),
-    path("auth/change-password/", ChangePasswordView.as_view(), name="change_password"),
+    path(
+        "auth/login/",
+        LoginView.as_view(),
+        name="login"
+    ),
+    path(
+        "auth/change-password/",
+        ChangePasswordView.as_view(),
+        name="change_password"
+    ),
+    path(
+        "auth/company/destinations/",
+        CompanyDestinationsView.as_view(),
+        name="company_destinations",
+    ),
 ]
